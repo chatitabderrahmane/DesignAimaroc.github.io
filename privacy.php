5beed01311be2463bfee917b9ff5969b3f495924
abderrahmane chatit <chatitabderrahmane@gmail.com>
2023-01-22 00:41:20 +0100

    Design ai maroc

---
+++ privacy.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="index.php">
+     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Cairo:wght@700&family=Rowdies:wght@300&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/icones/favicon.ico">
+
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <title>Privacy</title>
+</head>
+<body>
+<div class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+        <div class="container text-white ">
+             <a href="index.php" class="navbar-brand text-info"><img src="" alt="">Ai Au Maroc | الذكاء الاصطناعي <em><img style="width: 40px;" src="Images/icones/icon.png" alt=""></em></a>
+             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu" ><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse "id="mainmenu" >
+                 <ul class=" navbar-nav ms-auto">
+                    <li class="  nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="index.php#about" class="nav-link">À propos </a></li>
+                    <li class="nav-item"><a href="index.php#Gallerie" class="nav-link">Gallerie</a></li>
+                    <li class="nav-item"><a href="index.php#contact"class="nav-link">Contacter</a></li>
+                </ul>
+          </div>   
+        </div>
+    </div>
+    <div  >
+            <div class="py-5 container">
+            Designaimaroc@gmail.com     <h1 style="font-family: 'Cairo', sans-serif;" class="py-5 text-center">سياسة الخصوصية</h1>
+              <P class="lead text-center">
+              يتعهد موقع الذكاء الاصطناعي بالمغرب باحترام خصوصية الزوار بشكل كامل , قمنا بعمل هذه الخصوصيه حتى تكون على ثقه كامله بأننا سنحمي كل ما يتعلق بك و بخصوصيتك , وثيقة سياسة الخصوصية هذه تحدد الخطوط العريضة لأنواع المعلومات الشخصية التي سنهتم بها و نجمعها هنا في موقع الذكاء الاصطناعي بالمغرب.
+
+              </P>
+              <P class="lead text-center">
+                
+اذا كان لديك اي استفسار بخصوص هذه السياسه , نتمنى ان تراسلنا عبر هذا الايميل         
+       <a class="lead text-center d-block"href="">designaimaroc@gmail.com</a>
+              </P>
+              <P class="lead text-center">ما هي المعلومات التي يمكن ان نأخذها منك ؟؟</P>
+              <P class="lead text-center">في هذا الموقع , تأكد اننا لا نأخذ اي معلومه شخصيه منك دون علمك .. بل ان اي بيانات نحصل عليها تكون فقط بسبب انك انت من قام بتسجيلها بناء على رغبتك , للحصول على تقرير او تصميم او معلومات تهمك عبر الموقع .. و هذه البيانات تتضمن :
+</P>
+              <P class="lead text-center">
+              ايميلك الشخصي اسمك الأول رقم هاتفك اذا اردت تعديل اي معلومات قمت سابقا باعطائها لنا , فقط راسلنا عبر الايميل و سنعدلها لك متى ما شئت 
+              </P>
+              <P class="lead text-center">تعديل هذه السياسه كل فتره :</P>
+              <P class="lead text-center"> نعم , من الممكن ان نقوم بتعديل هذه السياسه بين الفتره و الاخرى , و هذا طبعا لمصلحتك اولاً و لتعرف كيف نحترم خصوصيتك بالشكل الصحيح , و عند قيامنا بذلك , نضع ملاحظة في الموقع بالتغيير لتدخل و تقرأ و تعرف حقوقك معنا   </P>
+              <P class="lead text-center">  الكوكيز و عدادات الانترنت :</P>
+              <P class="lead text-center"> ما يستخدم الكوكيز لحفظ المعلومات عن الإعدادات الخاصه بالزوار ، حيث تسجل هذه المعلومات عند دخول أي صفحه من صفحات موقعنا , و هذا لا يؤثر على اداءك او تصفحك ابدا , بل انه مفيد جدا في بعض الاحيان التي تتطلب دخول مستمر لحساباتك عبر الانترنت . </P>
+              <P class="lead text-center">قبول شروط الاستخدام : </P>
+              <P class="lead text-center">باستخدام هذا الموقع فإنك تقر أنك قرأت وفهمت، وقبلت، ومن ثم وافقت على الالتزام بهذه الشروط.  </P>
+            </div>
+    </div>
+      
+</body>
+</html>
---
+++ termofuse.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Cairo:wght@700&family=Rowdies:wght@300&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/icones/favicon.ico">
+
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <title>term of us</title>
+</head>
+<body>
+<div class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+        <div class="container text-white ">
+             <a href="index.php" class="navbar-brand text-info"><img src="" alt="">Ai Au Maroc | الذكاء الاصطناعي <em><img style="width: 40px;" src="Images/icones/icon.png" alt=""></em></a>
+             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu" ><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse "id="mainmenu" >
+                 <ul class=" navbar-nav ms-auto">
+                    <li class="  nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="index.php#about" class="nav-link">À propos </a></li>
+                    <li class="nav-item"><a href="index.php#Gallerie" class="nav-link">Gallerie</a></li>
+                    <li class="nav-item"><a href="index.php#contact"class="nav-link">Contacter</a></li>
+                   </ul>
+          </div>   
+        </div>
+    </div>
+
+
+    <section>
+        <div class="container py-5">
+        <h1 style="font-family: 'Cairo', sans-serif;" class="py-5 text-center">شروط الاستخدام</h1>
+              <p class="text-center">:شروط الاسخدام لأي زائر يدخل هذا الموقع</p> 
+             <p class="lead text-center">
+
+             مقدمة:   ان استخدامك له يعني موافقتك بشكل كامل على الشروط الأحكام التالية الخاصة به , لهذا فإننا نحب دائماً ان تقرأ هذه الصفحه لتعرف الشروط التي يجب ان تتقيدها لتتعامل مع هذا الموقع بشكل لا يخالف اي من سياساته.
+             </p>
+             <p class="lead text-center">
+              سياسة الخصوصيه : الذكاء الاصطناعي بالمغرب  يحترم سياسة الخصوصيه لديك , و لك كامل التحكم بمعلوماتك و لن يتم تجميعها الا باذنك و برغبتك انت طبعاً , و قد قمنا بعمل صفحه كامله تعرف فيها سياسة الخصوصيه الخاصه بهذا الموقع من خلال الضغط على هذا الرابط <a href="privacy.php"> .من هنا</a>  
+             </p>
+             <p class="lead text-center">
+الملكيه الفكريه :عبد الرحمان شعطيط هو وحده من يملك محتوى هذا الموقع بالكامل , من صور  و  خدمات … الخ , كل ماتراه في هذا الموقع من جهده الخاص هو و فريق عمله.
+العلامات التجاريه  ai au maroc لا تقبل وجود علامات تجاريه هنا على هذا الموقع , هذا يضر بمصلحة الزائر .. و لا يجوز لك عمل اي عمليه تهدف الى تسويق علامه تجاريه معينه , الا طبعا اذا استأذنت من صاحب الموقع و وافق هو على ذلك.
+             </p>
+             <p class="lead text-center">
+             لأي استفسار حول هذه الشروط , يرجى ارسال ايميل الى 
+             </p>
+             <P class="lead  text-center" ><a  href="#">designaimaroc@gmail.com</a></P>
+
+        </div>
+    </section>
+</body>
+</html>
